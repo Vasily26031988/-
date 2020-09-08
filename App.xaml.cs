200e8b4280f9b781bdc5b020ee1474a50e3364c6
Vasily26031988 <sator1988@gmail.com>
2020-09-08 22:28:32 +0400

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System.Globalization;
+using System.Windows.Markup;
+
+namespace Органайзер
+{
+	/// <summary>
+	/// Логика взаимодействия для App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			var cultureinfo = new CultureInfo("ru-RU");
+			Thread.CurrentThread.CurrentCulture = cultureinfo;
+			Thread.CurrentThread.CurrentUICulture = cultureinfo;
+			CultureInfo.DefaultThreadCurrentCulture = cultureinfo;
+			CultureInfo.DefaultThreadCurrentUICulture = cultureinfo;
+			FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+			base.OnStartup(e);
+		}
+	}
+}
